197a0a3df628e0df40939f6f4ae71f71f4a9d315
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-04-06 01:25:13 -0300

    ..
    .

---
+++ TodoTarefaVue2/todo-tarefas-2/src/components/statusTodo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "StatusTodo",
+  props: {
+    percentageTodo: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
